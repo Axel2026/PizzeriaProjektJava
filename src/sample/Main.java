c406cca0f9f11ee71c7702a10c868deeff8e71bb
Patryk Nytko <pnytko20@gmail.com>
2021-05-05 16:33:40 +0200

    Dodanie sceny z przyciskami startowymi

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,13 +6,18 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.File;
+
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("StartingButtons.fxml"));
+        primaryStage.setTitle("Na Okrągło");
+        primaryStage.setScene(new Scene(root, 1280, 720));
+        File file = new File("F:\\git_reps\\PizzeriaProjektJava\\src\\sample\\style.css");
+        root.getStylesheets().clear();
+        root.getStylesheets().add("file:///" + file.getAbsolutePath().replace("\\", "/"));
         primaryStage.show();
     }
 
